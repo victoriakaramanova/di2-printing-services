e40b929619abd2a3f5ec9287cf5ee46403720596
victoriakaramanova <37152322+victoriakaramanova@users.noreply.github.com>
2020-03-26 01:48:50 +0200

    Intruduce OrderSupplier

---
+++ Data/Di2.Data/Migrations/20200325211343_5.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Di2.Data.Migrations
+{
+    public partial class _5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderSuppliers_Materials_MaterialId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderSuppliers_Suppliers_SupplierId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderSuppliers_MaterialId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderSuppliers_SupplierId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.DropColumn(
+                name: "MaterialId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.DropColumn(
+                name: "UnitPrice",
+                table: "OrderSuppliers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriceListMaterialId",
+                table: "OrderSuppliers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriceListSupplierId",
+                table: "OrderSuppliers",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderSuppliers_PriceListMaterialId_PriceListSupplierId",
+                table: "OrderSuppliers",
+                columns: new[] { "PriceListMaterialId", "PriceListSupplierId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderSuppliers_PriceLists_PriceListMaterialId_PriceListSupplierId",
+                table: "OrderSuppliers",
+                columns: new[] { "PriceListMaterialId", "PriceListSupplierId" },
+                principalTable: "PriceLists",
+                principalColumns: new[] { "MaterialId", "SupplierId" },
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderSuppliers_PriceLists_PriceListMaterialId_PriceListSupplierId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderSuppliers_PriceListMaterialId_PriceListSupplierId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.DropColumn(
+                name: "PriceListMaterialId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.DropColumn(
+                name: "PriceListSupplierId",
+                table: "OrderSuppliers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaterialId",
+                table: "OrderSuppliers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SupplierId",
+                table: "OrderSuppliers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "UnitPrice",
+                table: "OrderSuppliers",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderSuppliers_MaterialId",
+                table: "OrderSuppliers",
+                column: "MaterialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderSuppliers_SupplierId",
+                table: "OrderSuppliers",
+                column: "SupplierId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderSuppliers_Materials_MaterialId",
+                table: "OrderSuppliers",
+                column: "MaterialId",
+                principalTable: "Materials",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderSuppliers_Suppliers_SupplierId",
+                table: "OrderSuppliers",
+                column: "SupplierId",
+                principalTable: "Suppliers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
